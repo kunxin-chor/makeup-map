3767ba0bf7551fbe5409f56203b11c490b0a5b68
kunxin-chor <chorkunxin@yahoo.com>
2025-04-03 13:29:17 +0000

    Added markers and circles

--- 02-layers/script.js
+++ 02-layers/script.js
@@ -1,5 +1,7 @@
 const singapore = [1.3521, 103.8198]; // coordinates for leaflet are lat, lng, and usually they are elements in array
-const map = L.map('map');
+const map = L.map('map', {
+    doubleClickZoom: false
+});
 
 // set the center point of the map
 map.setView(singapore, 13);
@@ -10,4 +12,30 @@ map.setView(singapore, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// Add a marker to our map
+const bugisMarker = L.marker([1.2994, 103.8555]);
+bugisMarker.addTo(map);
+
+bugisMarker.bindPopup(`<h1>Bugis</h1>
+    <img style="width:240px" src="https://upload.wikimedia.org/wikipedia/commons/8/83/Raffles_Hospital_in_Bugis%2C_Singapore.jpg"/>
+    `);
+
+const somersetMarker = L.marker([1.3010, 103.8386]).addTo(map);
+somersetMarker.addEventListener("click", function(){
+    alert("Somerset clicked");
+})
+
+map.on('dblclick', function(event){
+    // event contains all the details and parameters about the event
+    const latLng = event.latlng;
+    const newMarker = L.marker([latLng.lat, latLng.lng]);
+    newMarker.addTo(map);
+})
+
+L.circle([1.3294,103.8021], {
+    color: 'red',
+    fillColor: "orange",
+    radius: 500 // unit of measurement in meter
 }).addTo(map);
